f5246a0d0eaecd66f76d6fd3765d753f9562f593
Mike Marseglia <mike@marseglia.org>
2016-08-01 14:27:14 -0400

    Documentation update.

--- manifests/oracle.pp
+++ manifests/oracle.pp
@@ -6,11 +6,15 @@
 #
 # Install one or more versions of Oracle Java.
 #
-# uses the following to download the package and automatically accept
+# Uses the following URL to download the package and automatically accept
 # the licensing terms.
 # wget --no-cookies --no-check-certificate --header \
 # "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" \
 # "http://download.oracle.com/otn-pub/java/jdk/8u25-b17/jre-8u25-linux-x64.tar.gz"
+#
+# URL is constructed using the following variables:
+# 8u25-b17/jre-8u25-linux-x64.tar.gz
+# ${release_major}-{$release_minor}/${java_se}-${release_major}-${os}-${architecture}.tar.gz
 #
 # Parameters
 # [*version*]
